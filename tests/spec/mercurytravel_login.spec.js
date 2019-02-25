8593cfbb7ac7239e9170896d35e675f029eef27b
Matt Schlosser <mschlosser87@gmail.com>
2019-02-24 23:08:10 -0500

    Working login test plus addition of dotenv

--- tests/spec/mercurytravel_login.spec.js
+++ tests/spec/mercurytravel_login.spec.js
@@ -1,11 +1,15 @@
+require('dotenv').config();
 
 describe('Open Mercury Travels home page', function() {
   it('should allow the user to log in', function() {
     browser.url('http://newtours.demoaut.com/mercurywelcome.php');
     const signinlink = $('=SIGN-ON');
     signinlink.click();
-    console.log(signinlink.getText());
-    browser.pause(5000);
-    //browser.geturl().should.be.equal('http://newtours.demoaut.com/mercurysignon.php')
+    $('body').waitForExist(10000);
+    expect(browser.getUrl()).to.include('mercurysignon.php');
+    $('input[name="userName"]').addValue(process.env.MT_LOGIN);
+    $('input[name="password"]').addValue(process.env.MT_PASS);
+    $('input[name="login"]').click();
+    $('body').waitForExist(10000);
   });
 });
